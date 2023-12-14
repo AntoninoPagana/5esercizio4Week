466f32830258fefe002f8351c610c71b6b7a977a
Antonino <pagana899@gmail.com>
2023-12-14 17:54:21 +0100

    Esercizio Completo

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        controlloValori(null, 8);
-    }
-    public static void controlloValori(Integer numeratore, Integer denominatore) {
         try {
-            if (numeratore != null && denominatore != null) {
-                System.out.println("I valori inseriti non sono null!!");
-            }
-        } catch (Exception e) {
+            int risultato = controlloValori(10, 5);
+            System.out.println("Risultato divisione = " + risultato);
+        } catch (Exception e){
             System.out.println("Errore: " + e);
         }
+    }
+    public static int controlloValori(Integer numeratore, Integer denominatore) {
+        if(numeratore == null || denominatore == null){
+            throw new NullPointerException("I valori non devono essere null!");
+        }
+        if(denominatore == 0){
+            throw new ArithmeticException("Un numero non si puo dividere per 0");
+        }
+        return numeratore / denominatore;
     }
 }
